99731c2d5a2ba0469becb1fc1dbcab503ed4f5fd
Ahmad <ahmad@example.com>
2026-07-06 11:44:01 +0300

    Create Signup Route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -10,6 +11,7 @@ mongoose.connect(process.env.MONGO).then(() => {
     console.log("Error connecting to MongoDB", err);
 });
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
@@ -17,3 +19,4 @@ app.listen(3000, () => {
 );
 
 app.use("/api/users",userRouter);
+app.use("/api/auth",authRouter);
