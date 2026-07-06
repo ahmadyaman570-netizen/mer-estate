81a4b5061fd8bbacfc6a46cf5c53d690da66ca05
Ahmad <ahmad@example.com>
2026-07-06 13:54:31 +0300

    Create UI Sign up

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,15 +1,14 @@
-import User from '../models/user.model.js';
-import bcrypt from 'bcryptjs';
-import { errorHandler } from '../utils/error.js';
-export const signup = async (req, res,next) => {
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
-  const hashedPassword =bcrypt.hashSync(password, 10);
+  const hashedPassword = bcrypt.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
- try {
+  try {
     await newUser.save();
     res.status(201).json("User has been created successfully");
- }
-    catch (error) { 
-    next(error)
-    }
-}
+  } catch (error) {
+    next(error);
+  }
+};
--- api/index.js
+++ api/index.js
@@ -5,22 +5,25 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(() => {
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
     console.log("Connected to MongoDB");
-}).catch((err) => {
+  })
+  .catch((err) => {
     console.log("Error connecting to MongoDB", err);
-});
+  });
 const app = express();
 app.use(express.json());
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-}
-);
+  console.log("Server is running on port 3000");
+});
 
-app.use("/api/users",userRouter);
-app.use("/api/auth",authRouter);
+app.use("/api/users", userRouter);
+app.use("/api/auth", authRouter);
 app.use((err, req, res, next) => {
-   const statusCode=err.statusCode || 500;
-   const message=err.message || "Internal Server Error";
-   return res.status(statusCode).json({ success: false, statusCode, message }); });
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({ success: false, statusCode, message });
+});
