84cd62c17a169b788827447fe6f18ee814f92965
Ahmad <ahmad@example.com>
2026-07-06 15:36:01 +0300

    sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
+
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcrypt.hashSync(password, 10);
@@ -8,6 +10,25 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json("User has been created successfully");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"));
+    }
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(401, "Invalid password"));
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password:pass, ...otherDetails } = validUser._doc;
+    res.cookie("access_token", token, {httpOnly:true, expires: new Date(Date.now() + 3600000)}).status(200).json(otherDetails);
   } catch (error) {
     next(error);
   }
